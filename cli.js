c51657d880e287bc00359ba4f40e78c810c868d4
Carlos Cuesta <carloscuesta@me.com>
2016-11-18 00:08:04 +0100

    :construction: Add xo linting, update --list option

--- cli.js
+++ cli.js
@@ -3,38 +3,37 @@
 
 const meow = require('meow');
 const axios = require('axios');
-const chalk = require('chalk');
+
 const gitmojis = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json';
 
 const cli = meow(`
-    Usage
-      $ gitmoji
+	Usage
+	  $ gitmoji
 
-    Options
-        --init, -i  Create and initialize the gitmoji commit hook
-        --list, -l  Output all the gitmojis
+	Options
+		--init, -i  Create and initialize the gitmoji commit hook
+		--list, -l  Output all the gitmojis
 `, {
-    alias: {
-        i: 'init',
-        l: 'list'
-    }
+	alias: {
+		i: 'init',
+		l: 'list'
+	}
 });
 
 if (cli.flags.list) {
+	axios.get(gitmojis)
+		.then(res => {
+			const gitmojiList = res.data.gitmojis;
 
-axios.get(gitmojis)
-    .then(function (res) {
-        let gitmojiList = res.data.gitmojis;
+			const gitmojiPrettyList = gitmojiList.map(obj => {
+				let list = {};
+				list = `${obj.emoji}  - ${obj.code} - ${obj.description}`;
+				return list;
+			});
 
-        let gitmojiPrettyList = gitmojiList.map( (obj) => {
-            let list = {};
-            list = `${obj.emoji}  - ${obj.code} - ${obj.description}`;
-            return list;
-        });
-
-        console.log(gitmojiPrettyList);
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
+			console.log(gitmojiPrettyList);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 }
