9b712c6c36e727761ffbe208b9fe0cc4e9f2edaf
Yahya SayadArbabi <TheYahya@users.noreply.github.com>
2017-09-24 13:56:03 +0000

    :art: Update options formatting (#66)

--- cli.js
+++ cli.js
@@ -13,14 +13,14 @@ const cli = meow(`
   Usage
     $ gitmoji
   Options
-    --init, -i  Initialize gitmoji as a commit hook
-    --remove -r Remove a previously initialized commit hook
-    --config, -g Setup gitmoji-cli preferences.
-    --commit, -c Interactively commit using the prompts
-    --list, -l  List all the available gitmojis
-    --search, -s  Search gitmojis
-    --version, -v Print gitmoji-cli installed version
-    --update, -u  Sync emoji list with the repo
+    --init, -i      Initialize gitmoji as a commit hook
+    --remove, -r    Remove a previously initialized commit hook
+    --config, -g    Setup gitmoji-cli preferences.
+    --commit, -c    Interactively commit using the prompts
+    --list, -l      List all the available gitmojis
+    --search, -s    Search gitmojis
+    --version, -v   Print gitmoji-cli installed version
+    --update, -u    Sync emoji list with the repo
   Examples
     $ gitmoji -l
     $ gitmoji bug linter -s
