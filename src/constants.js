0476c7f628cb799033af0185dd3be3d717cc1454
Olivier Roche <olivierroche@MacBook-Pro-de-Olivier.local>
2018-07-25 17:18:23 +0200

    :children_crossing: Ignore hook when rebasing
    The hook breaks the git rebase process. Modified script to not apply the hook when we are in rebase mode. See https://gmurphey.github.io/2013/02/02/ignoring-git-hooks-when-rebasing.html#.W1iRxtgzYWo for details

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,12 @@ const CODE = 'code'
 const EMOJI_FORMAT = 'emojiFormat'
 const GITHUB = 'github'
 const HOOK_MODE = 'hook'
-const HOOK_FILE_CONTENTS = '#!/bin/sh\n# gitmoji as a commit hook\n' +
-  'exec < /dev/tty\ngitmoji --hook $1\n'
+const HOOK_FILE_CONTENTS = 'BRANCH_NAME=$(git branch | grep \'*\' | sed \'s/* //\')\n' +
+'if [ $BRANCH_NAME != \'(no branch)\' ]\n' +
+'then\n' +
+      '#!/bin/sh\n# gitmoji as a commit hook\n' +
+  'exec < /dev/tty\ngitmoji --hook $1\n' +
+'fi'
 const HOOK_PATH = '/hooks/prepare-commit-msg'
 const HOOK_PERMISSIONS = 0o775
 const ISSUE_FORMAT = 'issueFormat'
