37b3df061ae464b269f98d9f5c0eea9f05898702
Carlos Cuesta <carloscuesta@me.com>
2019-09-11 11:23:34 +0200

    ♻️ Reference prompt and setting (#102)
    * :fire: Remove reference prompt and setting
    
    Reference should be specified on the message input. This will allow to enter URLs reference and everything else.
    
    * :camera_flash: Match snapshots
    
    * :memo: Update README images

--- src/config.js
+++ src/config.js
@@ -5,14 +5,10 @@ const config = new Conf()
 
 const getAutoAdd = () => config.get(constants.AUTO_ADD)
 const getEmojiFormat = () => config.get(constants.EMOJI_FORMAT)
-const getIssueFormat = () => config.get(constants.ISSUE_FORMAT)
 const getSignedCommit = () => config.get(constants.SIGNED_COMMIT)
 const setAutoAdd = (autoAdd) => config.set(constants.AUTO_ADD, autoAdd)
 const setEmojiFormat = (emojiFormat) => {
   config.set(constants.EMOJI_FORMAT, emojiFormat)
-}
-const setIssueFormat = (issueFormat) => {
-  config.set(constants.ISSUE_FORMAT, issueFormat)
 }
 const setSignedCommit = (signedCommit) => {
   config.set(constants.SIGNED_COMMIT, signedCommit)
@@ -21,10 +17,8 @@ const setSignedCommit = (signedCommit) => {
 module.exports = {
   getAutoAdd,
   getEmojiFormat,
-  getIssueFormat,
   getSignedCommit,
   setAutoAdd,
   setEmojiFormat,
-  setIssueFormat,
   setSignedCommit
 }
--- src/guard.js
+++ src/guard.js
@@ -1,59 +1,15 @@
 const chalk = require('chalk')
 
-const constants = require('./constants')
-
 const errors = {
   title: chalk.red('Enter a valid commit title'),
-  message: chalk.red('Enter a valid commit message'),
-  referenceGithub: chalk.red(
-    'Enter the number of the reference without the #. Eg: 12'
-  ),
-  referenceJira: chalk.red('Enter the JIRA reference key, such as ABC-123')
+  message: chalk.red('Enter a valid commit message')
 }
 
 const title = (title) => (!title || title.includes('`')) ? errors.title : true
 
 const message = (message) => message.includes('`') ? errors.message : true
 
-/**
-* Checks that the given github reference is valid
-* https://help.github.com/articles/autolinked-references-and-urls/
-* @param {Number} ref - Represents a github reference
-* @returns {Boolean}
-*/
-const githubGuard = (ref) => ref.match(/(^[1-9][0-9]*)+$/)
-  ? true
-  : errors.referenceGithub
-
-/**
-* Checks that the given jira reference is valid
-* https://confluence.atlassian.com/jirasoftwarecloud/linking-issues-776997756.html
-* @param {Number} ref - Represents a jira reference
-* @returns {Boolean}
-*/
-const jiraGuard = (ref) => ref.match(/^([A-Z][A-Z0-9]{1,9}-[0-9]+)$/g)
-  ? true
-  : errors.referenceJira
-
-const reference = (reference, mode) => {
-  if (!reference) return true
-  switch (mode) {
-    case constants.GITHUB: {
-      return githubGuard(reference)
-    }
-
-    case constants.JIRA: {
-      return jiraGuard(reference)
-    }
-
-    default: {
-      return githubGuard(reference)
-    }
-  }
-}
-
 module.exports = {
   title,
-  message,
-  reference
+  message
 }
