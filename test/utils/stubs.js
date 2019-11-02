78073860c066bfb6fb9e2e1bd84dfef8b02abf38
Carlos Cuesta <carloscuesta@me.com>
2019-11-02 17:41:12 +0100

    :white_check_mark: Add findGitmojiCommand spec

---
+++ test/utils/stubs.js
@@ -0,0 +1,35 @@
+export const commands = [
+  'commit',
+  'config',
+  'hook',
+  'init',
+  'list',
+  'remove',
+  'search',
+  'update'
+]
+
+export const cliMock = (options) => ({
+  flags: {
+    commit: options.commit || false,
+    config: options.config || false,
+    hook: options.hook || false,
+    init: options.init || false,
+    list: options.list || false,
+    remove: options.remove || false,
+    search: options.search || false,
+    update: options.update || false
+  },
+  showHelp: jest.fn()
+})
+
+export const optionsMock = {
+  commit: jest.fn(),
+  config: jest.fn(),
+  hook: jest.fn(),
+  init: jest.fn(),
+  list: jest.fn(),
+  remove: jest.fn(),
+  search: jest.fn(),
+  update: jest.fn()
+}
