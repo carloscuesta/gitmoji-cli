fa06407a3e2542c77a89b66efc0287df77c545ed
Carlos Cuesta <carloscuesta@me.com>
2019-11-02 15:29:04 +0100

    :white_check_mark: Add emojisCache spec

---
+++ test/utils/emojisCache.spec.js
@@ -0,0 +1,82 @@
+import pathExists from 'path-exists'
+import fs from 'fs'
+import path from 'path'
+
+import emojisCache, {
+  GITMOJI_CACHE,
+  CACHE_PATH
+} from '../../src/utils/emojisCache'
+import * as stubs from './stubs'
+
+jest.mock('os', () => ({
+  homedir: jest.fn().mockReturnValue('/Users/Test/')
+}))
+
+describe('emojisCache', () => {
+  it('should match GITMOJI_CACHE', () => {
+    expect(GITMOJI_CACHE).toMatchSnapshot()
+  })
+
+  it('should match CACHE_PATH', () => {
+    expect(CACHE_PATH).toMatchSnapshot()
+  })
+
+  describe('isAvailable', () => {
+    emojisCache.isAvailable()
+
+    it('should check that cache path exists', () => {
+      expect(pathExists.sync).toHaveBeenCalledWith(CACHE_PATH)
+    })
+  })
+
+  describe('createEmojis', () => {
+    describe('when cache directory does not exists', () => {
+      beforeAll(() => {
+        pathExists.sync.mockReturnValue(false)
+        emojisCache.createEmojis(stubs.gitmojis)
+      })
+
+      it('should create cache directory', () => {
+        expect(fs.mkdirSync).toHaveBeenCalledWith(path.dirname(CACHE_PATH))
+      })
+
+      it('should write the gitmojis.json file', () => {
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+          CACHE_PATH,
+          JSON.stringify(stubs.gitmojis)
+        )
+      })
+    })
+
+    describe('when cache directory exists', () => {
+      beforeAll(() => {
+        pathExists.sync.mockReturnValue(true)
+        fs.mkdirSync.mockClear()
+        fs.writeFileSync.mockClear()
+        emojisCache.createEmojis(stubs.gitmojis)
+      })
+
+      it('should create cache directory', () => {
+        expect(fs.mkdirSync).not.toHaveBeenCalled()
+      })
+
+      it('should write the gitmojis.json file', () => {
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+          CACHE_PATH,
+          JSON.stringify(stubs.gitmojis)
+        )
+      })
+    })
+  })
+
+  describe('getEmojis', () => {
+    beforeAll(() => {
+      fs.readFileSync.mockReturnValue(JSON.stringify(stubs.gitmojis))
+      emojisCache.getEmojis()
+    })
+
+    it('should read the emojis from the cache', () => {
+      expect(fs.readFileSync).toHaveBeenCalled()
+    })
+  })
+})
