ba35d8c640cfa6d593ed89f37e54b18f84b57f68
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2019-12-24 09:48:28 +0100

    :children_crossing: Prevent hook to be run when rebasing

--- src/commands/hook/hook.js
+++ src/commands/hook/hook.js
@@ -1,10 +1,27 @@
 // @flow
+
+const CONTENTS = `#! /bin/bash
+# gitmoji as a commit hook
+
+# strict mode for bash
+set -eo pipefail
+IFS=$'\\n\\t'
+
+readonly COMMIT_MSG_FILE=$1
+# no undefined variables
+set -u
+
+BRANCH_NAME="$(git branch | sed -e 's/* //')"
+if [[ ! "$BRANCH_NAME" =~ 'no branch' ]]; then
+  exec < /dev/tty
+  gitmoji --hook "$COMMIT_MSG_FILE"
+fi
+`
+
 const HOOK: Object = {
   PERMISSIONS: 0o775,
   PATH: '/hooks/prepare-commit-msg',
-  CONTENTS:
-    '#!/bin/sh\n# gitmoji as a commit hook\n' +
-    'exec < /dev/tty\ngitmoji --hook $1\n'
+  CONTENTS
 }
 
 export default HOOK
