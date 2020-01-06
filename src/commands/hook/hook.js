d8ec4d11502425fa1eecde290ad797f8c876e86b
Hugues Lerebours <hugues@lereboursp.net>
2020-01-06 18:51:12 +0100

    :bug: Ignore hook if not creating a new commit (#247)
    * 🎨 (hook.js): Use template string for multi-line string
    * 🐛 (hook.js): Ignore hook if not creating a commit from tty
    
    Co-authored-by: Carlos Cuesta <carloscuesta@me.com>

--- src/commands/hook/hook.js
+++ src/commands/hook/hook.js
@@ -2,9 +2,14 @@
 const HOOK: Object = {
   PERMISSIONS: 0o775,
   PATH: '/hooks/prepare-commit-msg',
-  CONTENTS:
-    '#!/bin/sh\n# gitmoji as a commit hook\n' +
-    'exec < /dev/tty\ngitmoji --hook $1\n'
+  CONTENTS: `#!/bin/sh
+# gitmoji as a commit hook
+if test -t 1 && ! grep -q -m 1 '^[^#]' $1; then
+  # it has been invoked from a tty and no commit message is already set
+  exec < /dev/tty
+  gitmoji --hook $1
+fi
+`
 }
 
 export default HOOK
