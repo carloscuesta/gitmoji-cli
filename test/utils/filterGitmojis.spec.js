4bbc71a3affd027d0699930510ef7dda86d31ae0
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-02-03 10:55:38 +0100

    :bug: Improve fuzzy search (#303)
    * :children_crossing: Fix fuzzy search threshold for better matching
    
    * :white_check_mark: Improve filterGitmojis tests
    
    The tests now use real life examples

--- test/utils/filterGitmojis.spec.js
+++ test/utils/filterGitmojis.spec.js
@@ -8,9 +8,35 @@ describe('filterGirmojis', () => {
     expect(filteredGitmojis).toStrictEqual(stubs.gitmojis)
   })
 
-  it('should should find heart gitmoji', () => {
-    const filteredGitmojis = filterGitmojis('hart', stubs.gitmojis)
+  it('should should find the `Updating tests.` gitmoji first', () => {
+    const filteredGitmojis = filterGitmojis('test', stubs.gitmojis)
 
-    expect(filteredGitmojis[0]).toStrictEqual(stubs.gitmojis[0])
+    const gitmoji = stubs.gitmojis.find(
+      (gitmoji) => gitmoji.name === 'white-check-mark'
+    )
+    expect(filteredGitmojis[0]).toStrictEqual(gitmoji)
+  })
+
+  it('should should find the `Introducing new features.` gitmoji first', () => {
+    const filteredGitmojis = filterGitmojis('fea', stubs.gitmojis)
+
+    const gitmoji = stubs.gitmojis.find(
+      (gitmoji) => gitmoji.name === 'sparkles'
+    )
+    expect(filteredGitmojis[0]).toStrictEqual(gitmoji)
+  })
+
+  it('should should find the `Moving or renaming files.` gitmoji first', () => {
+    const filteredGitmojis = filterGitmojis('move', stubs.gitmojis)
+
+    const gitmoji = stubs.gitmojis.find((gitmoji) => gitmoji.name === 'truck')
+    expect(filteredGitmojis[0]).toStrictEqual(gitmoji)
+  })
+
+  it('should should find the `Updating code due to external API changes.` gitmoji first', () => {
+    const filteredGitmojis = filterGitmojis('alian', stubs.gitmojis)
+
+    const gitmoji = stubs.gitmojis.find((gitmoji) => gitmoji.name === 'alien')
+    expect(filteredGitmojis[0]).toStrictEqual(gitmoji)
   })
 })
