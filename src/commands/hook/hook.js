c668e779d4866188c7fe8a3b7be8d56309e065d0
Carlos Cuesta <carloscuesta@me.com>
2020-03-11 17:44:16 +0100

    :ambulance: Revert hook content changes'

--- src/commands/hook/hook.js
+++ src/commands/hook/hook.js
@@ -2,24 +2,9 @@
 const HOOK: Object = {
   PERMISSIONS: 0o775,
   PATH: '/hooks/prepare-commit-msg',
-  CONTENTS: `#!/bin/sh
-# gitmoji as a commit hook
-commit_message_empty=true
-while read line
-do
-  if [[ $line == "# ------------------------ >8 ------------------------"* ]]; then
-    break
-  elif [[ $line != "#"* && $line != "" ]]; then
-    commit_message_empty=false
-    break
-  fi
-done < $1
-
-if test -t 1 && $commit_message_empty; then
-  # it has been invoked from a tty and no commit message is already set
-  exec < /dev/tty
-  gitmoji --hook $1
-fi`
+  CONTENTS:
+    '#!/bin/sh\n# gitmoji as a commit hook\n' +
+    'exec < /dev/tty\ngitmoji --hook $1\n'
 }
 
 export default HOOK
