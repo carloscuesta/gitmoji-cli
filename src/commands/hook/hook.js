0c911e58c3c663894fe17db276457367f3791e61
Nathan Poirier <nathan@poirier.io>
2020-04-28 23:28:50 +0200

    :ok_hand: Re-add hook content changes

--- src/commands/hook/hook.js
+++ src/commands/hook/hook.js
@@ -2,9 +2,29 @@
 const HOOK: Object = {
   PERMISSIONS: 0o775,
   PATH: '/hooks/prepare-commit-msg',
-  CONTENTS:
-    '#!/bin/sh\n# gitmoji as a commit hook\n' +
-    'exec < /dev/tty\ngitmoji --hook $1\n'
+  CONTENTS: `#!/bin/sh
+# gitmoji as a commit hook
+commit_message_empty=true
+while read -r line
+do
+  case "$line" in
+  "# ------------------------ >8 ------------------------"*)
+    break
+    ;;
+  ""|"#"*)
+    # skip
+    ;;
+  *)
+    commit_message_empty=false
+    break
+    ;;
+  esac
+done < "$1"
+if test -t 1 && $commit_message_empty; then
+  # it has been invoked from a tty and no commit message is already set
+  exec < /dev/tty
+  gitmoji --hook "$1"
+fi`
 }
 
 export default HOOK
