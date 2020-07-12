c8329d87d0effc79d067f1fcd6e20d035f46c468
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-07-12 21:02:34 +0200

    🚸 Silence the hook when the commit message is set

--- src/commands/commit/withHook/index.js
+++ src/commands/commit/withHook/index.js
@@ -26,4 +26,16 @@ export const registerHookInterruptionHandler = () => {
   })
 }
 
+export const cancelIfMessageIsAlreadySet = () => {
+  const commitMessageFilePath = process.argv[3]
+  if (
+    fs.existsSync(commitMessageFilePath) &&
+    fs.lstatSync(commitMessageFilePath).isFile() &&
+    !!fs.readFileSync(commitMessageFilePath, 'utf8')
+  ) {
+    console.warn('A commit message is already set, cancelling gitmoji\n')
+    process.exit(0)
+  }
+}
+
 export default withHook
