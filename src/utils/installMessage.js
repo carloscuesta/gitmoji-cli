a8f4d1b4814a95e1165ad4e3af2fadb856ab9169
Carlos Cuesta <carloscuesta@me.com>
2020-08-10 22:17:52 +0200

    :heart: Add postinstall message

---
+++ src/utils/installMessage.js
@@ -0,0 +1,8 @@
+// @flow
+import chalk from 'chalk'
+
+console.log(chalk`
+  Hey 👋. Thanks for using {yellow gitmoji-cli} 😜
+  Feel free to {bold support the development} of this cli using the following link:
+  👉 {blue https://github.com/sponsors/carloscuesta} ❤️
+`)
