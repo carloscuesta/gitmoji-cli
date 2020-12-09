62be69f4aeccccf66ca199f65f9141e9e482d031
Carlos Cuesta <carloscuesta@me.com>
2020-12-09 09:24:40 +0100

    :wrench: Remove installMessage util
    Fixes https://github.com/carloscuesta/gitmoji-cli/issues/506

--- src/utils/installMessage.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import chalk from 'chalk'
-
-console.log(chalk`
-  Hey 👋. Thanks for using {yellow gitmoji-cli} 😜
-  Feel free to {bold support the development} of this cli using the following link:
-  👉 {blue https://github.com/sponsors/carloscuesta} ❤️
-`)
