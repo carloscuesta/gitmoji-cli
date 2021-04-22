1aaf1ed4aa59349ceea3d50029e2389da0b317d8
thtliife <vito.giarrusso@me.com>
2021-04-22 13:12:00 +1000

    🐛 (flowtypes): fix bad flow type for buildFetchOptions param

--- src/utils/buildFetchOptions.js
+++ src/utils/buildFetchOptions.js
@@ -5,11 +5,11 @@ const defaultProxy =
   process.env.https_proxy || process.env.http_proxy || undefined
 
 export const buildAgent = (proxy: string = defaultProxy) =>
-  proxy ? new ProxyAgent(proxy) : undefined
+  proxy ? new ProxyAgent(proxy) : ''
 
-export const buildFetchOptions = (options: object = {}) => {
+export const buildFetchOptions = (options: { proxy?: string } = {}) => {
   const agent = buildAgent(options.proxy)
-  return { ...options, agent }
+  return { agent }
 }
 
 export default buildFetchOptions
