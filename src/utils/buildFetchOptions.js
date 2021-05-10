bf38c65b65062ef64a046a5f742db1b845c7d148
Vito Giarrusso <vito.giarrusso@me.com>
2021-05-11 05:12:10 +1000

    Add proxy support for node-fetch (#603)
    * :sparkles: add proxy support for node-fetch
    * :white_check_mark: (getEmoji.spec): add proxy agent to getEmoji spec
    * :white_check_mark: (node-fetch): add spec for buildFetchOptions
    * :bug: (flow): switch to maybe types for possibly undefined types
    turns out flow != typescript ;)
    * :recycle: Organize tests
    
    Co-authored-by: Carlos Cuesta <carloscuesta@me.com>

---
+++ src/utils/buildFetchOptions.js
@@ -0,0 +1,17 @@
+// @flow
+import ProxyAgent from 'proxy-agent'
+
+const defaultProxy: ?string =
+  process.env.https_proxy || process.env.http_proxy || undefined
+
+export const buildAgent = (proxy: ?string = defaultProxy): ?ProxyAgent =>
+  proxy ? new ProxyAgent(proxy) : undefined
+
+export const buildFetchOptions = (
+  options: { proxy?: string } = {}
+): ?{ agent: Object } => {
+  const agent = buildAgent(options.proxy)
+  return agent ? { agent } : undefined
+}
+
+export default buildFetchOptions
---
+++ test/utils/buildFetchOptions.spec.js
@@ -0,0 +1,206 @@
+import buildFetchOptions from '../../src/utils/buildFetchOptions'
+
+describe('buildFetchOptions', () => {
+  describe('when proxy is passed as an argument', () => {
+    describe('when using an anonymous proxy', () => {
+      describe('when is http proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          expect(
+            buildFetchOptions({ proxy: 'http://localhost:8080' })
+          ).toMatchObject({
+            agent: {
+              proxy: {
+                auth: null,
+                host: 'localhost:8080',
+                hostname: 'localhost',
+                href: 'http://localhost:8080/',
+                port: '8080',
+                protocol: 'http:'
+              },
+              proxyUri: 'http://localhost:8080'
+            }
+          })
+        })
+      })
+
+      describe('when is https proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          expect(
+            buildFetchOptions({ proxy: 'https://localhost:8080' })
+          ).toMatchObject({
+            agent: {
+              proxy: {
+                auth: null,
+                host: 'localhost:8080',
+                hostname: 'localhost',
+                href: 'https://localhost:8080/',
+                port: '8080',
+                protocol: 'https:'
+              },
+              proxyUri: 'https://localhost:8080'
+            }
+          })
+        })
+      })
+    })
+
+    describe('when using an authenticated proxy', () => {
+      describe('when is http proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          expect(
+            buildFetchOptions({ proxy: 'http://user:pass@localhost:8080' })
+          ).toMatchObject({
+            agent: {
+              proxy: {
+                auth: 'user:pass',
+                host: 'localhost:8080',
+                hostname: 'localhost',
+                href: 'http://user:pass@localhost:8080/',
+                port: '8080',
+                protocol: 'http:'
+              },
+              proxyUri: 'http://user:pass@localhost:8080'
+            }
+          })
+        })
+      })
+
+      describe('when is https proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          expect(
+            buildFetchOptions({ proxy: 'https://user:pass@localhost:8080' })
+          ).toMatchObject({
+            agent: {
+              proxy: {
+                auth: 'user:pass',
+                host: 'localhost:8080',
+                hostname: 'localhost',
+                href: 'https://user:pass@localhost:8080/',
+                port: '8080',
+                protocol: 'https:'
+              },
+              proxyUri: 'https://user:pass@localhost:8080'
+            }
+          })
+        })
+      })
+    })
+  })
+
+  describe('when proxy is detected from the enviroment', () => {
+    const OLD_ENV = process.env
+
+    beforeEach(() => {
+      jest.resetModules()
+      process.env = { ...OLD_ENV }
+      process.env.http_proxy = undefined
+      process.env.https_proxy = undefined
+    })
+
+    afterAll(() => {
+      process.env = OLD_ENV
+    })
+
+    describe('when using an anonymous proxy', () => {
+      describe('when is http proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          process.env.http_proxy = 'http://localhost-env:8080'
+
+          const buildFetchOptionsFromEnv = require('../../src/utils/buildFetchOptions')
+            .default
+
+          expect(buildFetchOptionsFromEnv()).toMatchObject({
+            agent: {
+              proxy: {
+                auth: null,
+                host: 'localhost-env:8080',
+                hostname: 'localhost-env',
+                href: 'http://localhost-env:8080/',
+                port: '8080',
+                protocol: 'http:'
+              },
+              proxyUri: 'http://localhost-env:8080'
+            }
+          })
+        })
+      })
+
+      describe('when is https proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          process.env.https_proxy = 'https://localhost-env:8080'
+
+          const buildFetchOptionsFromEnv = require('../../src/utils/buildFetchOptions')
+            .default
+
+          expect(buildFetchOptionsFromEnv()).toMatchObject({
+            agent: {
+              proxy: {
+                auth: null,
+                host: 'localhost-env:8080',
+                hostname: 'localhost-env',
+                href: 'https://localhost-env:8080/',
+                port: '8080',
+                protocol: 'https:'
+              },
+              proxyUri: 'https://localhost-env:8080'
+            }
+          })
+        })
+      })
+    })
+
+    describe('when using an authenticated proxy', () => {
+      describe('when is http proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          process.env.http_proxy = 'http://user:pass@localhost-env:8080'
+
+          const buildFetchOptionsFromEnv = require('../../src/utils/buildFetchOptions')
+            .default
+
+          expect(buildFetchOptionsFromEnv()).toMatchObject({
+            agent: {
+              proxy: {
+                auth: 'user:pass',
+                host: 'localhost-env:8080',
+                hostname: 'localhost-env',
+                href: 'http://user:pass@localhost-env:8080/',
+                port: '8080',
+                protocol: 'http:'
+              },
+              proxyUri: 'http://user:pass@localhost-env:8080'
+            }
+          })
+        })
+      })
+
+      describe('when is https proxy', () => {
+        it('should return an object containing a proxy agent', () => {
+          process.env.https_proxy = 'https://user:pass@localhost-env:8080'
+
+          const buildFetchOptionsFromEnv = require('../../src/utils/buildFetchOptions')
+            .default
+
+          expect(buildFetchOptionsFromEnv()).toMatchObject({
+            agent: {
+              proxy: {
+                auth: 'user:pass',
+                host: 'localhost-env:8080',
+                hostname: 'localhost-env',
+                href: 'https://user:pass@localhost-env:8080/',
+                port: '8080',
+                protocol: 'https:'
+              },
+              proxyUri: 'https://user:pass@localhost-env:8080'
+            }
+          })
+        })
+      })
+    })
+  })
+
+  describe('when no proxy is detected or used', () => {
+    it('should return undefined', () => {
+      expect(buildFetchOptions()).toBe(undefined)
+    })
+  })
+})
