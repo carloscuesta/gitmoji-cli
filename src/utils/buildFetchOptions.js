de6de24d2ad9b653e9d59cd0a2f4b3e1fb32a699
thtliife <vito.giarrusso@me.com>
2021-05-10 13:08:23 +1000

    :art: (proxy-agent): only build agent if proxy is required

--- src/utils/buildFetchOptions.js
+++ src/utils/buildFetchOptions.js
@@ -1,14 +1,18 @@
 // @flow
-import ProxyAgent from 'proxy-agent'
+import ProxyAgent from 'proxy-agent';
 
-const defaultProxy = process.env.https_proxy || process.env.http_proxy || ''
+const defaultProxy: string | undefined =
+  process.env.https_proxy || process.env.http_proxy || undefined;
 
-export const buildAgent = (proxy: string = defaultProxy) =>
-  proxy ? new ProxyAgent(proxy) : ''
+export const buildAgent = (
+  proxy: string | undefined = defaultProxy
+): ProxyAgent | undefined => (proxy ? new ProxyAgent(proxy) : undefined);
 
-export const buildFetchOptions = (options: { proxy?: string } = {}) => {
-  const agent = buildAgent(options.proxy)
-  return { agent }
-}
+export const buildFetchOptions = (
+  options: { proxy?: string } = {}
+): { agent: Object | undefined } => {
+  const agent = buildAgent(options.proxy);
+  return agent ? { agent } : undefined;
+};
 
-export default buildFetchOptions
+export default buildFetchOptions;
