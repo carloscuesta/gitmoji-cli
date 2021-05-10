df884159e2f1349b538d295459b3cac7d476c673
thtliife <vito.giarrusso@me.com>
2021-05-10 14:17:29 +1000

    :bug: (flow): switch to maybe types for possibly undefined types
    turns out flow != typescript ;)

--- src/utils/buildFetchOptions.js
+++ src/utils/buildFetchOptions.js
@@ -1,16 +1,15 @@
 // @flow
 import ProxyAgent from 'proxy-agent'
 
-const defaultProxy: string | undefined =
+const defaultProxy: ?string =
   process.env.https_proxy || process.env.http_proxy || undefined
 
-export const buildAgent = (
-  proxy: string | undefined = defaultProxy
-): ProxyAgent | undefined => (proxy ? new ProxyAgent(proxy) : undefined)
+export const buildAgent = (proxy: ?string = defaultProxy): ?ProxyAgent =>
+  proxy ? new ProxyAgent(proxy) : undefined
 
 export const buildFetchOptions = (
   options: { proxy?: string } = {}
-): { agent: Object | undefined } => {
+): ?{ agent: Object } => {
   const agent = buildAgent(options.proxy)
   return agent ? { agent } : undefined
 }
