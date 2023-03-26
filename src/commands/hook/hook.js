d7016acad105efff6a3bcf83e38e6f991e3e2a8b
Andy Augustin <dev@andreas-augustin.org>
2023-03-26 22:03:13 +0200

    :sparkles: (hook): (#1038) add npx to hook command
    with npx it is easier to use a  in a protable way

--- src/commands/hook/hook.js
+++ src/commands/hook/hook.js
@@ -4,7 +4,7 @@ const HOOK: Object = {
   FILENAME: 'prepare-commit-msg',
   CONTENTS:
     '#!/bin/sh\n# gitmoji as a commit hook\n' +
-    'exec < /dev/tty\ngitmoji --hook $1 $2\n'
+    'exec < /dev/tty\nnpx gitmoji --hook $1 $2\n'
 }
 
 export default HOOK
