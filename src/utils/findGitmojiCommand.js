f8bc88d62b389143c99d7111c673a270c297e215
segersniels <segers.n@hotmail.com>
2023-05-05 17:28:17 +0200

    :sparkles: Add support for subcommands alongside flag execution

--- src/utils/findGitmojiCommand.js
+++ src/utils/findGitmojiCommand.js
@@ -2,6 +2,10 @@
 import COMMIT_MODES from '@constants/commit'
 import FLAGS from '@constants/flags'
 
+const isSupportedCommand = (command: ?string, options: Object): boolean => {
+  return Object.keys(options).includes(command)
+}
+
 const getOptionsForCommand = (command: ?string, flags: Object): ?Object => {
   const commandsWithOptions = [FLAGS.COMMIT, FLAGS.HOOK]
 
@@ -19,14 +23,25 @@ const getOptionsForCommand = (command: ?string, flags: Object): ?Object => {
 
 const findGitmojiCommand = (cli: any, options: Object): void => {
   const flags = cli.flags
-  const commandFlag = Object.keys(flags)
+
+  // Attempt to get the command out of the flags
+  let command = Object.keys(flags)
     .map((flag) => flags[flag] && flag)
     .find((flag) => options[flag])
-  const commandOptions = getOptionsForCommand(commandFlag, flags)
 
-  return options[commandFlag]
-    ? options[commandFlag](commandOptions)
-    : cli.showHelp()
+  if (!command && cli.input.length) {
+    // See if the user provided the command as a subcommand
+    command = cli.input[0]
+  }
+
+  // Unable to determine requested command so display help
+  if (!command || !isSupportedCommand(command, options)) {
+    return cli.showHelp()
+  }
+
+  const commandOptions = getOptionsForCommand(command, flags)
+
+  return options[command] ? options[command](commandOptions) : cli.showHelp()
 }
 
 export default findGitmojiCommand
