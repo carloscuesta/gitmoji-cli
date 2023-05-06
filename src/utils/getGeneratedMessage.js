9e4a98f755ea1c78c38cea0c9f495d509390f434
segersniels <segers.n@hotmail.com>
2023-05-06 20:00:55 +0200

    :bug: Make sure we properly respect total amount of processed tokens

--- src/utils/getGeneratedMessage.js
+++ src/utils/getGeneratedMessage.js
@@ -6,13 +6,19 @@ import chalk from 'chalk'
 import configurationVault from '@utils/configurationVault'
 import { EMOJI_COMMIT_FORMATS } from '@constants/configuration'
 
-const MAX_TOKEN_LENGTH = 500
-
 export type ParsedMessage = {
   gitmoji: string,
   title: string,
   raw: string
 }
+
+const MAX_RESPONSE_LENGTH = 500
+const GPT4_TOKEN_LENGTH = 8192
+const GPT3_TOKEN_LENGTH = 4096
+
+const getMaxTokenLength = (model: 'gpt-3.5-turbo' | 'gpt-4') =>
+  (model === 'gpt-3.5-turbo' ? GPT3_TOKEN_LENGTH : GPT4_TOKEN_LENGTH) -
+  MAX_RESPONSE_LENGTH
 
 /**
  * Do some additional post processing on the received answer
@@ -55,17 +61,18 @@ const getGeneratedMessage = async (
   diff: string,
   gitmojis: Object[],
   context?: string,
-  model: string = 'gpt-3.5-turbo'
+  model: 'gpt-3.5-turbo' | 'gpt-4' = 'gpt-3.5-turbo'
 ): Promise<ParsedMessage | void> => {
   const encoding = encoding_for_model(model)
-  let prompt = getPrompt(diff, gitmojis, context)
+  const modelLength = getMaxTokenLength(model)
 
   // Check if exceeding model max token length and minify accordingly
-  if (encoding.encode(prompt).length > 4096) {
+  let prompt = getPrompt(diff, gitmojis, context)
+  if (encoding.encode(prompt).length > modelLength) {
     prompt = getPrompt(diff, gitmojis, context, true)
 
     // Check if minified prompt is still too long
-    if (encoding.encode(prompt).length > 4096) {
+    if (encoding.encode(prompt).length > modelLength) {
       return console.error(
         chalk.red(
           'The diff is too large, try reducing the number of staged changes.'
@@ -87,7 +94,7 @@ const getGeneratedMessage = async (
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
-      max_tokens: MAX_TOKEN_LENGTH
+      max_tokens: MAX_RESPONSE_LENGTH
     })
   })
 
