fc4eaaaff28cdb40ae183071636b574e6f9cb244
segersniels <segers.n@hotmail.com>
2023-05-07 10:59:35 +0200

    :white_check_mark: Use test.each instead

--- test/utils/findGitmojiCommand.spec.js
+++ test/utils/findGitmojiCommand.spec.js
@@ -4,40 +4,36 @@ import COMMIT_MODES from '../../src/constants/commit'
 import * as stubs from './stubs'
 
 describe('findGitmojiCommand', () => {
-  stubs.commands.map((command) => {
-    if ([FLAGS.COMMIT, FLAGS.HOOK].includes(command)) {
-      describe(`with ${command}`, () => {
-        it(`it should match command`, () => {
-          findGitmojiCommand(
-            stubs.cliMock({ [command]: true }),
-            stubs.optionsMock
-          )
-          expect(stubs.optionsMock[command]).toHaveBeenCalledWith({
-            mode:
-              command === FLAGS.HOOK ? COMMIT_MODES.HOOK : COMMIT_MODES.CLIENT,
-            message: undefined,
-            scope: undefined,
-            title: undefined
-          })
-        })
-      })
-    }
+  describe('with option', () => {
+    test.each(stubs.commands)('it should match %s option', (command) => {
+      findGitmojiCommand(stubs.cliMock({ [command]: true }), stubs.optionsMock)
+      expect(stubs.optionsMock[command]).toHaveBeenCalled()
+    })
+  })
 
-    describe('with options', () => {
-      it(`it should match ${command} command`, () => {
+  describe('with command', () => {
+    test.each(stubs.commands)('it should match %s command', (command) => {
+      findGitmojiCommand(stubs.cliMock({}, [command]), stubs.optionsMock)
+      expect(stubs.optionsMock[command]).toHaveBeenCalled()
+    })
+  })
+
+  describe('with specific command', () => {
+    test.each([FLAGS.COMMIT, FLAGS.HOOK])(
+      'it should have been called with',
+      (command) => {
         findGitmojiCommand(
           stubs.cliMock({ [command]: true }),
           stubs.optionsMock
         )
-        expect(stubs.optionsMock[command]).toHaveBeenCalled()
-      })
-    })
-
-    describe('with command', () => {
-      it(`it should match ${command} command`, () => {
-        findGitmojiCommand(stubs.cliMock({}, [command]), stubs.optionsMock)
-        expect(stubs.optionsMock[command]).toHaveBeenCalled()
-      })
-    })
+        expect(stubs.optionsMock[command]).toHaveBeenCalledWith({
+          mode:
+            command === FLAGS.HOOK ? COMMIT_MODES.HOOK : COMMIT_MODES.CLIENT,
+          message: undefined,
+          scope: undefined,
+          title: undefined
+        })
+      }
+    )
   })
 })
