0c3f030beac1d93e8450f9229e5de3f49a2903db
Niels <segers.n@hotmail.com>
2023-05-20 07:01:31 +0200

    🐛 Filter out `search` from query when using subcommands (#1075)
    * :bug: Filter out subcommand from search inputs
    
    undefined
    
    * :white_check_mark: Add test to ensure that we filter out the subcommand when searching
    
    undefined
    
    ---------
    
    Co-authored-by: Carlos Cuesta <carloscuesta@me.com>

--- src/utils/findGitmojiCommand.js
+++ src/utils/findGitmojiCommand.js
@@ -6,34 +6,66 @@ const isSupportedCommand = (command: ?string, options: Object): boolean => {
   return Object.keys(options).includes(command)
 }
 
-const getOptionsForCommand = (command: ?string, flags: Object): ?Object => {
-  const commandsWithOptions = [FLAGS.COMMIT, FLAGS.HOOK]
+const determineCommand = (
+  flags: Object,
+  input: string[],
+  options: Object
+): {
+  type: 'flag' | 'command',
+  command: ?string
+} => {
+  const command = Object.keys(flags)
+    .map((flag) => flags[flag] && flag)
+    .find((flag) => options[flag])
 
-  if (commandsWithOptions.includes(command)) {
-    return {
-      message: flags['message'],
-      mode: command === FLAGS.HOOK ? COMMIT_MODES.HOOK : COMMIT_MODES.CLIENT,
-      scope: flags['scope'],
-      title: flags['title']
-    }
+  return command
+    ? {
+        type: 'flag',
+        command
+      }
+    : {
+        type: 'command',
+        command: input[0]
+      }
+}
+
+const getOptionsForCommand = (
+  command: ?string,
+  flags: Object,
+  input: string[],
+  type: 'flag' | 'command'
+): ?Object => {
+  switch (command) {
+    case FLAGS.COMMIT:
+    case FLAGS.HOOK:
+      return {
+        message: flags['message'],
+        mode: command === FLAGS.HOOK ? COMMIT_MODES.HOOK : COMMIT_MODES.CLIENT,
+        scope: flags['scope'],
+        title: flags['title']
+      }
+    case FLAGS.SEARCH:
+      return {
+        query: type === 'command' ? input.slice(1) : input
+      }
   }
 
   return null
 }
 
 const findGitmojiCommand = (cli: any, options: Object): void => {
-  const flags = cli.flags
-
-  const command =
-    Object.keys(flags)
-      .map((flag) => flags[flag] && flag)
-      .find((flag) => options[flag]) || cli.input[0]
+  const { command, type } = determineCommand(cli.flags, cli.input, options)
 
   if (!command || !isSupportedCommand(command, options)) {
     return cli.showHelp()
   }
 
-  const commandOptions = getOptionsForCommand(command, flags)
+  const commandOptions = getOptionsForCommand(
+    command,
+    cli.flags,
+    cli.input,
+    type
+  )
 
   return options[command] ? options[command](commandOptions) : cli.showHelp()
 }
