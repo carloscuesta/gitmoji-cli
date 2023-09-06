59abb67bc981ff30b385bab6e94ca6dbb303b7bc
Marius Tempelmeier <cptRaclette@users.noreply.github.com>
2023-09-06 17:24:31 +0200

    :arrow_up: Upgrades proxy-agent to v.6

--- src/utils/buildFetchOptions.js
+++ src/utils/buildFetchOptions.js
@@ -1,18 +1,9 @@
 // @flow
-import ProxyAgent from 'proxy-agent'
+import { ProxyAgent } from 'proxy-agent'
 
-const defaultProxy: ?string =
-  process.env.https_proxy || process.env.http_proxy || undefined
-
-export const buildAgent = (
-  proxy: ?string = defaultProxy
-): ?typeof ProxyAgent => (proxy ? new ProxyAgent(proxy) : undefined)
-
-export const buildFetchOptions = (
-  options: { proxy?: string } = {}
-): ?{ agent: Object } => {
-  const agent = buildAgent(options.proxy)
-  return agent ? { agent } : undefined
+export const buildFetchOptions = (): ?{ agent: Object } => {
+  const isProxyConfigured = process.env.https_proxy || process.env.http_proxy
+  return isProxyConfigured ? { agent: new ProxyAgent() } : undefined
 }
 
 export default buildFetchOptions
