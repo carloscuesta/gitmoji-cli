eb95a1e649d62132ba011bf68fce2982f4416f01
segersniels <segers.n@hotmail.com>
2024-03-21 00:18:58 +0100

    :white_check_mark: Properly mock the dynamic imports

--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -1,11 +1,22 @@
 import updateNotifier from 'update-notifier'
 import meow from 'meow'
 
+import commit from '@commands/commit/index.js'
+import config from '@commands/config/index.js'
+import hook from '@commands/hook/index.js'
+import list from '@commands/list/index.js'
+import search from '@commands/search/index.js'
+import update from '@commands/update/index.js'
+
 import pkg from '../package.json'
 import { options } from '../src/cli.js'
-import commands from '../src/commands/index.js'
 
-jest.mock('../src/commands')
+jest.mock('@commands/commit/index.js')
+jest.mock('@commands/config/index.js')
+jest.mock('@commands/hook/index.js')
+jest.mock('@commands/list/index.js')
+jest.mock('@commands/search/index.js')
+jest.mock('@commands/update/index.js')
 
 describe('cli', () => {
   it('should call updateNotifier', () => {
@@ -16,50 +27,48 @@ describe('cli', () => {
   })
 
   it('should match meow with cli information', () => {
-    (
-      (meow as jest.Mock).mock.calls[0][1] as { importMeta: string }
-    ).importMeta = 'import.meta.url'
-
-    expect((meow as jest.Mock).mock.calls).toMatchSnapshot()
+    const mockMeow = meow as jest.Mock
+    mockMeow.mock.calls[0][1].importMeta = 'import.meta.url'
+    expect(mockMeow.mock.calls).toMatchSnapshot()
   })
 
-  it('should call commit command on commit', () => {
-    options.commit({ mode: 'client' })
-    expect(commands.commit).toHaveBeenCalledWith({ mode: 'client' })
+  it('should call commit command on commit', async () => {
+    await options.commit({ mode: 'client' })
+    expect(commit).toHaveBeenCalledWith({ mode: 'client' })
   })
 
-  it('should call config command on config', () => {
-    options.config()
-    expect(commands.config).toHaveBeenCalledWith()
+  it('should call config command on config', async () => {
+    await options.config()
+    expect(config).toHaveBeenCalledWith()
   })
 
-  it('should call commit command on hook', () => {
-    options.hook({ mode: 'hook' })
-    expect(commands.commit).toHaveBeenLastCalledWith({ mode: 'hook' })
+  it('should call commit command on hook', async () => {
+    await options.hook({ mode: 'hook' })
+    expect(commit).toHaveBeenLastCalledWith({ mode: 'hook' })
   })
 
-  it('should call createHook command on init', () => {
-    options.init()
-    expect(commands.createHook).toHaveBeenCalledWith()
+  it('should call createHook command on init', async () => {
+    await options.init()
+    expect(hook.create).toHaveBeenCalledWith()
   })
 
-  it('should call list command on list', () => {
-    options.list()
-    expect(commands.list).toHaveBeenCalledWith()
+  it('should call list command on list', async () => {
+    await options.list()
+    expect(list).toHaveBeenCalledWith()
   })
 
-  it('should call removeHook command on remove', () => {
-    options.remove()
-    expect(commands.removeHook).toHaveBeenCalledWith()
+  it('should call removeHook command on remove', async () => {
+    await options.remove()
+    expect(hook.remove).toHaveBeenCalledWith()
   })
 
-  it('should call search command on search', () => {
-    options.search({ query: ['testSearchQuery'] })
-    expect(commands.search).toHaveBeenCalledWith({ query: ['testSearchQuery'] })
+  it('should call search command on search', async () => {
+    await options.search({ query: ['testSearchQuery'] })
+    expect(search).toHaveBeenCalledWith({ query: ['testSearchQuery'] })
   })
 
-  it('should call update command on update', () => {
-    options.update()
-    expect(commands.update).toHaveBeenCalledWith()
+  it('should call update command on update', async () => {
+    await options.update()
+    expect(update).toHaveBeenCalledWith()
   })
 })
