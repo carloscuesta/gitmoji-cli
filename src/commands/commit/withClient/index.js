ac26d76514de6fde77eb8029c5adde09d2b0ed97
Miguel Zarco Gillett <mzarcogillett@gmail.com>
2024-03-21 21:41:01 +0100

    🚸 Warn user if they are not in a git repository (#1276)
    * 🚸 Warn user if they are not in a git repository
    
    The change introduces a check to bail out if the user runs the tool outside a git repository. It will also warn the user. This handles this common pitfall gracefully instead of logging the stdout errors that happen in these cases.
    
    * 🚸 Handles execa errors in a generic way
    
    ---------
    
    Co-authored-by: Miguel Zarco <miguel-antonio.zarco-gillett@lht.dlh.de>

--- src/commands/commit/withClient/index.js
+++ src/commands/commit/withClient/index.js
@@ -41,12 +41,10 @@ const withClient = async (answers: Answers): Promise<void> => {
   } catch (error) {
     console.error(
       chalk.red(
-        '\n',
-        'Oops! An error occurred. There is likely additional logging output above.\n',
-        'You can run the same commit with this command:\n'
-      ),
-      '\t',
-      error.escapedCommand
+        error,
+        '\n\n',
+        'Oops! An error occurred. There is likely additional logging output above.\n'
+      )
     )
   }
 }
